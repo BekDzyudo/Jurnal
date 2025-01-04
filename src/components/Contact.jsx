ed8cb1c832e6acead21b1f1d99b99dace936fe95
BekDzyudo <jalilovshohbek9909@gmail.com>
2025-01-04 20:30:49 +0500

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import qr from "../assets/qrSVG.svg"
 import { Link } from 'react-router-dom'
 import { MdMarkEmailUnread } from "react-icons/md";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { useTranslation } from 'react-i18next';
+import Calendar from 'react-calendar';
+import "react-calendar/dist/Calendar.css"
+import "./contact.css"
 
 function Contact() {
     const {t} = useTranslation()
+    const [value, onChange] = useState(new Date());
   return (
     <section id='contact' className='bg-[#dcefe4] mb-10 md:mb-24'>
         <div className='align-elements xl:py-24 md:py-14 py-8 flex flex-col md:flex-row justify-end items-center'>
+                <div className='calendarContainer md:w-[20%] w-52'>
+                    <Calendar value={value} onChange={onChange} className="calendar"/>
+                </div>
             <div className='md:w-[80%] flex flex-col items-center mb-8 md:mb-0'>
                 <h2 className='xl:text-5xl md:text-3xl text-xl mb-4 md:mb-10 font-semibold text-[#28c66f]'>{t('home_page.contactTitle')}</h2>
                 <p className='xl:text-2xl md:text-lg text-center opacity-80 mb-4 md:mb-10'>{t('home_page.contactDesc')}</p>
